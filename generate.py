1247d7c627a10e97d0ebcf2d232a6268d632063a
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-09-06 10:27:51 -0700

    fixing 18bit depth errors

--- generate.py
+++ generate.py
@@ -1,9 +1,18 @@
 from fpdf import FPDF
 import os
+from PIL import Image
+
+def convert_to_8bit(file):
+    img = Image.open("uploads/IMG_0317_Small.png")
+    img = img.convert("RGB")  # Convert to RGB (8-bit per channel)
+    img.save("uploads/IMG_0317_Small_8bit.png")
+    return("uploads/IMG_0317_Small_8bit.png")
 
 def pngtopdf(file):
+    new_file = convert_to_8bit(file)
     pdf = FPDF()
     pdf.add_page()
-    pdf.image(file, x=10, y=10, w=190, h=277)
+    pdf.image(new_file, x=10, y=10, w=190, h=277)
     pdf.output("pdf-with-image.pdf")
+    os.remove(new_file)
     return()
